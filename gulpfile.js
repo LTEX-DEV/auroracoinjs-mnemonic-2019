248bad288a3f70d90f3743044f7c85888bcf3537
digicontributer <esot@digibyte.io>
2018-04-15 21:14:12 +1000

    digibyte-mnemonic

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var gulp_bitcore = require('bitcore-build');
-
+var gulp_bitcore = require('digibyte-build');
+var gulp = require('gulp');
+console.log(gulp_bitcore)
 gulp_bitcore('mnemonic');
 
+gulp.task('default', ['browser']);
